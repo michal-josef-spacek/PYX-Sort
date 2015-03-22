c05ab843f9bdcd3dd7927a26789199ad5312ca49
Michal Špaček <tupinek@gmail.com>
2015-03-22 01:15:25 +0100

    Prepare for 0.03 version.

--- Sort.pm
+++ Sort.pm
@@ -9,7 +9,7 @@ use Class::Utils qw(set_params);
 use PYX::Parser;
 
 # Version.
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -221,6 +221,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
