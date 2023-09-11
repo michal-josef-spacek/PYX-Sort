3494bb78c33aa86f38a9d72be3c5372be23e68c2
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-09-11 18:09:39 +0200

    Improve LICENSE AND COPYRIGHT section in doc

--- Sort.pm
+++ Sort.pm
@@ -211,8 +211,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2011-2023 Michal Josef Špaček
- BSD 2-Clause License
+© 2011-2023 Michal Josef Špaček
+
+BSD 2-Clause License
 
 =head1 VERSION
 
